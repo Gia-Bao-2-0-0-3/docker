b8e913c3f4ceb259d412ad6403e49faa26fc9682
Gia-Bao-2-0-0-3 <baoliengianguyen@gmail.com>
2025-04-01 17:17:04 +0700

    bai 1 -> bai 8

---
+++ Phan2/Bai8/app/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 3000;
+
+const db = mysql.createConnection({
+  host: 'mysql',
+  user: 'root',
+  password: 'rootpassword',
+  database: 'testdb'
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Lỗi kết nối MySQL:', err);
+  } else {
+    console.log('Kết nối MySQL thành công!');
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Node.js & MySQL chạy trên Docker!');
+});
+
+app.listen(port, () => {
+  console.log(`Server đang chạy tại http://localhost:${port}`);
+});
